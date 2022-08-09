6dc3c37aeda195b5fedd4b5715eb1962147019b1
Rachid Calazans <rachidcalazans@gmail.com>
2022-08-09 14:39:57 -0300

    adicionado junit test
    Signed-off-by: Rachid Calazans <rachidcalazans@gmail.com>

--- 01Junit/test/JunitTest.java
+++ 01Junit/test/JunitTest.java
@@ -1,30 +1,16 @@
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class JunitTest {
     @Test
-    void comoDeclarar() {
-        // Declare uma variável usando <tipo> <nome>
-        int inteiro;
-
-        // Declare múltiplas variáveis em uma única linha
-        // <tipo> <nome1>, <nome2>, <nome3>
-        int inteiro1, inteiro2, inteiro3;
+    void primeiroTesteSucesso() {
+        // primeiro é a expectativa
+        // segundo é o resultado
+        Assertions.assertEquals(true, 1 == 1);
     }
 
     @Test
-    void comoInicializar() {
-        // Inicializa uma variável usando <tipo> <nome> = <val>
-        int inteiro = 1;
-        Assertions.assertEquals(1, inteiro);
-
-        // Inicializa múltiplas variáveis com mesmo valor
-        // declaração <tipo> <nome1>, <nome2>, <nome3>
-        // inicialização <name1> = <name2> = <name3> = <val>
-        int inteiro1, inteiro2, inteiro3;
-        inteiro1 = inteiro2 = inteiro3 = 1;
-
-        Assertions.assertEquals(1, inteiro1);
-        Assertions.assertEquals(1, inteiro2);
-        Assertions.assertEquals(1, inteiro3);
+    void segundoTesteFalha() {
+        Assertions.assertEquals(true, 1 == 2);
     }
 }
