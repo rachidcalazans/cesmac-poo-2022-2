79cd99e612df6897a03f43963132164358e6e9fb
Cesmac <cesmac@academico.cesmac.edu.br>
2022-08-10 19:07:16 -0300

    exercicio 02

--- 01jeyunit/teste/jayunitTest.java
+++ 01jeyunit/teste/jayunitTest.java
@@ -1,2 +1,21 @@
-package PACKAGE_NAME;public class jayunitTest {
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class jayunitTest {
+
+    @Test
+    void primeiroTesteSucesso(){
+        String name = "Arthur";
+        Assertions.assertEquals( "Arthur", name);
+    }
+    @Test
+    void segundoTestFalha(){
+        String lastname = "Sarmento";
+        Assertions.assertEquals( "Arthur", lastname);
+    }
+    @Test
+    void terceiroTest(){
+        String name = "Kenzo";
+        Assertions.assertEquals( "Kenzo", name);
+    }
 }
