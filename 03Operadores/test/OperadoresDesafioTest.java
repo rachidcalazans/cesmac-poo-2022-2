c2a348bcc9f212af6f5bc4667c9be52eda3dcc43
Cesmac <cesmac@academico.cesmac.edu.br>
2022-08-30 21:22:31 -0300

    Add Desafio + Exercicio Estrutura Controle
    Signed-off-by: Cesmac <cesmac@academico.cesmac.edu.br>

--- 03Operadores/test/OperadoresDesafioTest.java
+++ 03Operadores/test/OperadoresDesafioTest.java
@@ -5,12 +5,29 @@ public class OperadoresDesafioTest {
     // Desafio
     // Vale 1.5 pontos válido se criar PR até 17/08/2022 23:59:59
     // Implementar o método #calculadora
-    // Adicionar assert_equal para cada método de teste com seu respectivo operador
+    // Adicionar assert_equal para cada método de
+    // teste com seu respectivo operador
     Integer calculadora(Integer valorA, Integer valorB, String operador) {
+        if (operador.equals("+")) {
+            return valorA + valorB;
+        } else if (operador.equals("-")) {
+            return valorA - valorB;
+        } else if (operador.equals("*")) {
+            return valorA * valorB;
+        } else if (operador.equals("**")) {
+            return (int)Math.pow(valorA, valorB);
+        } else if (operador.equals("/")) {
+            return valorA / valorB;
+        } else if (operador.equals("%")) {
+            return valorA % valorB;
+        }
         return 0;
     }
 
     Double calculadora(Double valorA, Double valorB, String operador) {
+        if (operador.equals("/")) {
+            return valorA / valorB;
+        }
         return 0.0;
     }
 
@@ -21,21 +38,27 @@ public class OperadoresDesafioTest {
 
     @Test
     void subtracao() {
+        Assertions.assertEquals(1, calculadora(2, 1, "-"));
     }
 
     @Test
     void multiplicacao() {
+        Assertions.assertEquals(2, calculadora(2, 1, "*"));
     }
 
     @Test
     void potencia() {
+        Assertions.assertEquals(4, calculadora(2, 2, "**"));
     }
 
     @Test
     void divisao() {
+        Assertions.assertEquals(1, calculadora(2, 2, "/"));
+        Assertions.assertEquals(0.5, calculadora(1.0, 2.0, "/"));
     }
 
     @Test
     void modulo() {
+        Assertions.assertEquals(1, calculadora(5, 2, "%"));
     }
 }
--- 04EstruturasDeControle/test/EstruturasDeControleExercicioTest.java
+++ 04EstruturasDeControle/test/EstruturasDeControleExercicioTest.java
@@ -1,8 +1,38 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class EstruturasDeControleExercicioTest {
-    // - Funcionalidade - Retornar nome do mês do ano para qualquer número
-    // do mês passado. Caso seja um número inválido, retornar message de erro
+    // - Funcionalidade - Retornar nome do mês do ano
+    // para qualquer número
+    // do mês passado. Caso seja um número inválido,
+    // retornar message de erro
 
     /* -----------------
      * TODO
      * ----------------- */
+
+    String mesPorInt(int mes) {
+        if (mes < 1 || mes > 12)
+            return "Mes invalido";
+
+        String[] meses = {"Jan", "Fev", "Mar", "Abr", "Mai", "Jun", "Jul", "Ago", "Set", "Out", "Nov", "Dez"};
+
+        return meses[--mes];
+    }
+
+    @Test
+    void mesValido() {
+        String[] meses = {"Jan", "Fev", "Mar", "Abr", "Mai", "Jun", "Jul", "Ago", "Set", "Out", "Nov", "Dez"};
+        int mesAtual = 0;
+
+        for (String mes : meses) {
+            Assertions.assertEquals(mes, mesPorInt(++mesAtual));
+        }
+    }
+
+    @Test
+    void mesInvalid() {
+        Assertions.assertEquals("Mes invalido", mesPorInt(0));
+        Assertions.assertEquals("Mes invalido", mesPorInt(13));
+    }
 }
