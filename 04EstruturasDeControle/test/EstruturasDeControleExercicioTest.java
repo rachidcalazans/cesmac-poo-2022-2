17f49de33d0811e820ccc1d354914bedeb590146
JoaoVictorPimentel <joaovitorcavalcante009@gmail.com>
2022-09-01 01:23:27 -0300

    Exercício 05

--- 04EstruturasDeControle/test/EstruturasDeControleExercicioTest.java
+++ 04EstruturasDeControle/test/EstruturasDeControleExercicioTest.java
@@ -5,71 +5,53 @@ public class EstruturasDeControleExercicioTest {
     // - Funcionalidade - Retornar nome do mês do ano para qualquer número
     // do mês passado. Caso seja um número inválido, retornar message de erro
 
-    /* -----------------
-     * TODO
-     * ----------------- */
-
-    String mesPor
+    String mesPorString(int mes) {
+        if (mes == 1) {
+            return "Janeiro";
+        } else if (mes == 2) {
+            return "Fevereiro";
+        } else if (mes ==3) {
+            return "Março";
+        } else if (mes == 4) {
+            return "Abril";
+        } else if (mes == 5) {
+            return "Maio";
+        } else if (mes == 6) {
+            return "Junho";
+        } else if (mes == 7) {
+            return "Julho";
+        } else if (mes == 8) {
+            return "Agosto";
+        } else if (mes == 9) {
+            return "Setembro";
+        } else if (mes == 10) {
+            return "Outubro";
+        } else if (mes == 11) {
+            return "Novembro";
+        } else if (mes == 12) {
+            return "Dezembro";
+        } else {
+            return "Número não corresponde a nenhum mês";
+        }
+    }
 
     @Test
     void mesesAno(){
-        int mes = 1;
-        String mesDigitado;
+        Assertions.assertEquals("Janeiro", mesPorString(1));
+        Assertions.assertEquals("Fevereiro", mesPorString(2));
+        Assertions.assertEquals("Março", mesPorString(3));
+        Assertions.assertEquals("Abril", mesPorString(4));
+        Assertions.assertEquals("Maio", mesPorString(5));
+        Assertions.assertEquals("Junho", mesPorString(6));
+        Assertions.assertEquals("Julho", mesPorString(7));
+        Assertions.assertEquals("Agosto", mesPorString(8));
+        Assertions.assertEquals("Setembro", mesPorString(9));
+        Assertions.assertEquals("Outubro", mesPorString(10));
+        Assertions.assertEquals("Novembro", mesPorString(11));
+        Assertions.assertEquals("Dezembro", mesPorString(12));
 
-        switch (mes) {
-            case 1:
-                mesDigitado = "Janeiro";
-                break;
-
-            case 2:
-                mesDigitado = "Fevereiro";
-                break;
-
-            case 3:
-                mesDigitado = "Março";
-                break;
-
-            case 4:
-                mesDigitado = "Abril";
-                break;
-
-            case 5:
-                mesDigitado = "Maio";
-                break;
-
-            case 6:
-                mesDigitado = "Junho";
-                break;
-
-            case 7:
-                mesDigitado = "Julho";
-                break;
-
-            case 8:
-                mesDigitado = "Agosto";
-                break;
-
-            case 9:
-                mesDigitado = "Setembro";
-                break;
-
-            case 10:
-                mesDigitado = "Outubro";
-                break;
-
-            case 11:
-                mesDigitado = "Novembro";
-                break;
-
-            case 12:
-                mesDigitado = "Dezembro";
-                break;
-
-            default:
-                mesDigitado = "Mês inválido";
-                break;
-        }
-
-        System.out.println(mesDigitado);
+        Assertions.assertEquals("Número não corresponde a nenhum mês", mesPorString(0));
+        Assertions.assertEquals("Número não corresponde a nenhum mês", mesPorString(13));
+        Assertions.assertEquals("Número não corresponde a nenhum mês", mesPorString(99));
     }
 }
