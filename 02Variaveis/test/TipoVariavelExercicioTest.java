203fa0e0c92a355355c13364fe7eef82a930bd45
CarlosFilipeMS <carlosfilipe_2506@hotmail.com>
2022-09-06 08:34:19 -0300

    enviando exercico 03

--- 02Variaveis/test/TipoVariavelExercicioTest.java
+++ 02Variaveis/test/TipoVariavelExercicioTest.java
@@ -1,3 +1,5 @@
+package test;
+
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
@@ -5,27 +7,27 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class TipoVariavelExercicioTest {
+
+    // exercicio 003 aqui
     @Test
     void exercicio01() {
-        boolean boolean1 = false;
-        Boolean boolean2 = true;
+        boolean boolean1 = true;
+        Boolean boolean2 = false;
 
         Assertions.assertEquals(true, boolean1);
         Assertions.assertEquals(false, boolean2);
 
         Assertions.assertNotEquals(boolean1, boolean2);
     }
-
     @Test
     void exercicio02() {
         int inteiro = 2;
-        Integer inteiro2 = 3;
+        Integer inteiro2 = 2;
 
         Assertions.assertEquals(inteiro, inteiro2);
         Assertions.assertEquals(inteiro2, inteiro2.intValue());
         Assertions.assertEquals(String.valueOf(inteiro), inteiro2.toString());
     }
-
     @Test
     void exercicio03() {
         float flutuante = 1.1f;
@@ -35,36 +37,35 @@ public class TipoVariavelExercicioTest {
         Double flutuante4 = 1.1;
 
         Assertions.assertNotEquals(flutuante, flutuante2);
-        Assertions.assertEquals(flutuante, flutuante3);
-        Assertions.assertEquals(flutuante, flutuante4);
+        Assertions.assertNotEquals(flutuante, flutuante3);
+        Assertions.assertNotEquals(flutuante, flutuante4);
     }
-
     @Test
     void exercicio04() {
         String string1 = "Uma nova string";
         String string2 = "Uma outra string" + "com" + "oncatenação";
 
-        Assertions.assertEquals("Uma nova string!", string1);
-        Assertions.assertEquals("Uma outra string com oncatenação", string2);
+        Assertions.assertEquals("Uma nova string", string1);
+        Assertions.assertEquals("Uma outra string" + "com" + "oncatenação", string2);
     }
-
     @Test
     void exercicio05() {
         int[] intArray = new int[4];
 
-        String nomes[] = {"Zé", "Roberto", "Mara", "Joana"};
+        String nomes[] = {"Zé", "Roberto", "Maria", "Joana"};
 
-        Assertions.assertEquals(2, intArray.length);
+        Assertions.assertEquals(4, intArray.length);
 
         Assertions.assertEquals(4, nomes.length);
         Assertions.assertEquals("Zé", nomes[0]);
         Assertions.assertEquals("Roberto", nomes[1]);
         Assertions.assertEquals("Maria", nomes[2]);
-        Assertions.assertEquals("Joana", nomes[4]);
+        Assertions.assertEquals("Joana", nomes[3]);
 
         List<String> nomes2 = new ArrayList<String>() {
             {
                 add("Zé");
+                add("Roberto");
             }
         };
         Assertions.assertEquals(2, nomes2.size());
@@ -73,9 +74,11 @@ public class TipoVariavelExercicioTest {
 
         List<String> nomes3 = new ArrayList<String>();
         nomes3.add("Maria");
+        nomes3.add("Joana");
 
         Assertions.assertEquals(2, nomes3.size());
-        Assertions.assertEquals("Maria", nomes3);
-        Assertions.assertEquals("Joana", nomes3);
+        Assertions.assertEquals("Maria", nomes3.get(0));
+        Assertions.assertEquals("Joana", nomes3.get(1));
     }
 }
+
