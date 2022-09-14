87fe398daab8b1c850a75801cb90e7374100c102
GabrielRosendoL <gabrielrosendoleitesilva@gmail.com>
2022-09-14 15:42:22 -0300

    Branch original do Exercício 06

--- 05Classes/src/Fogao.java
+++ 05Classes/src/Fogao.java
@@ -2,4 +2,58 @@ public class Fogao {
     public int bocas;
     private String marca;
     private boolean ligado = false;
+    // Adições
+    private boolean ligar;
+    private boolean desligar;
+
+    ////////////
+
+    public String getMarca(){
+        return "";
+    }
+
+    ////////////
+
+    public int getBocas(){
+        return 0;
+    }
+
+    ////////////
+
+    public boolean estaLigado(){
+        return ligado;
+    }
+
+    ////////////
+
+    public Fogao(){
+        this.marca = getMarca();
+        this.bocas = getBocas();
+    }
+
+    ////////////
+
+    public Fogao(int bocas, String marca){
+        this.bocas = getBocas();
+        this.marca = getMarca();
+    }
+
+    ////////////
+
+    public Fogao(boolean ligado){
+        this.ligar = estaLigado();
+        this.desligar = estaLigado();
+    }
+
+    ////////////
+
+    public void ligar(){
+        this.ligado = true;
+    }
+
+    ////////////
+
+    public void desligar(){
+        this.ligado = false;
+    }
 }
