b8824e631af56ee28391322a257f85d8ad007976
Cesmac <cesmac@academico.cesmac.edu.br>
2022-09-13 21:26:14 -0300

    resolução exercicio 03 sobre classes

--- 05Classes/src/Fogao.java
+++ 05Classes/src/Fogao.java
@@ -1,5 +1,33 @@
-public class Fogao {
+import java.security.PublicKey;
+
+public class Fogao{
     public int bocas;
     private String marca;
     private boolean ligado = false;
+
+
+    public String getMarca() {
+        return marca;
+    }
+    public int getBocas(){
+        return bocas;
+    }
+    public boolean estaLigado() {
+        return ligado;
+    }
+    public Fogao(){
+        this.bocas = getBocas();
+        this.marca = getMarca();
+    }
+    public Fogao(int bocas, String marca){
+        this.bocas = getBocas();
+        this.marca = getMarca();
+    }
+    public void ligar(){
+        this.ligado = true;
+    }
+    public void desligar(){
+        this.ligado = false;
+    }
+
 }
