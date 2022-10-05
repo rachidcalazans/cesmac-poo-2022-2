445251c2a6b6244621113d22d3349a9dd9d6dc91
Cesmac <cesmac@academico.cesmac.edu.br>
2022-10-05 19:45:27 -0300

    Arthur Kenzo: Tentei criar a arraylist mas sinceramente nem sei se deu certo k kk k

--- Dojo/src/carros.java
+++ Dojo/src/carros.java
@@ -3,4 +3,14 @@ public class carros {
     private int preco;
     private int saldo;
 }
+public class loja {
+    public static void loja(String[] args) {
+        ArrayList<String> estoque = new ArrayList<String>();
+        estoque.add("corolla");
+        estoque.add("corolla");
+        estoque.add("yaris");
+        estoque.add("yaris");
+        estoque.add("yaris");
 
+    }
+}
