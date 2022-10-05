bda5a3a7f32c3f9b86c1e206dc82ac7c348253bb
Cesmac <cesmac@academico.cesmac.edu.br>
2022-10-05 19:53:00 -0300

    GaVictor Hugo: iniciei a criança£o de métodos e iniciei o construtor de venda.

--- Dojo/src/Carro.java
+++ Dojo/src/Carro.java
@@ -2,8 +2,19 @@ public class Carro {
     public String modelo;
     public double preco;
 
-    public Car(String modelo, Double preco){
 
+    public String getModelo(){
+        return modelo;
+    }
+
+    public double getPreco(){ return preco;}
+    public Carro(){
+        this.modelo = getModelo();
+        this.preco = getPreco();
+    }
+    public Carro(String modelo, Double preco ){
+        this.modelo = getModelo();
+        this.preco = getPreco();
     }
 
 }
--- Dojo/test/ConcessionariaTest.java
+++ Dojo/test/ConcessionariaTest.java
@@ -9,4 +9,6 @@ public class ConcessionariaTest {
 
         Assertions.assertEquals(00,00);
     }
+    @Test
+    void VendaCarro
 }
