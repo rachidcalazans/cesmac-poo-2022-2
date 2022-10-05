4a7d5facbc1edbc8d66535b7eb8b0649d338c8aa
Cesmac <cesmac@academico.cesmac.edu.br>
2022-10-05 19:55:47 -0300

    ana: coloquei o array na classe carros

--- Dojo/ToyotaCarros/ToyotaCarros.java
+++ Dojo/ToyotaCarros/ToyotaCarros.java
@@ -1,2 +1,3 @@
-package PACKAGE_NAME;public class ToyotaCarros {
+public class ToyotaCarros {
+
 }
--- Dojo/ToyotaTest/ToyotTest.java
+++ Dojo/ToyotaTest/ToyotTest.java
@@ -4,8 +4,7 @@ import org.junit.jupiter.api.Test;
 public class ToyotTest {
     @Test
     void estadoInicialConcessionaria(){
-
-
+        public String[] carros = {"Corolla1", "Corolla2", "Yaris1", "Yaris2", "Yaris3"};
 
     }
 }
