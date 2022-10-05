c609ec6da3b6894cd8016048282d7860141eda0b
Cesmac <cesmac@academico.cesmac.edu.br>
2022-10-05 19:07:30 -0300

    Lucas Fidelis: Criei a src Carros, depois a pasta de testes e começamos a iniciar o projeto em si

---
+++ Dojo/Test/carrosTeste.java
@@ -0,0 +1,15 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class carrosTeste {
+    @Test
+    void estadoInicial(){
+        Assertions.assertEquals("");
+    }
+
+
+
+
+
+
+}
---
+++ Dojo/src/carros.java
@@ -0,0 +1,5 @@
+public class carros {
+    public String marca;
+    public String preco;
+    public int saldo;
+}
