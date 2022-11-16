d1a5ee730eea90ec3d4390b6f3f785e57ca810f4
Cesmac <cesmac@academico.cesmac.edu.br>
2022-11-16 19:49:55 -0300

    Início da Prova 03

---
+++ Prova03/src/Evento.java
@@ -0,0 +1,24 @@
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class Evento {
+    public static void main(String[] args) {
+        try {
+
+            // Lê o arquivo
+            FileReader ler = new FileReader("Prova03/proposals.txt");
+            BufferedReader reader = new BufferedReader(ler);
+            String linha;
+            while( (linha = reader.readLine()) != null ){
+                System.out.println(linha);
+            }
+
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
