3827aab3c4caf1c6239b7ea6182f49ca7b8d84ad
Cesmac <cesmac@academico.cesmac.edu.br>
2022-11-16 19:48:29 -0300

    Dei continuidade criando uma SRC de Evento

--- Prova03/src/Evento.java
+++ Prova03/src/Evento.java
@@ -1,2 +1,23 @@
-package PACKAGE_NAME;public class Evento {
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class Evento {
+    public static void main(String[] args) {
+        try {
+
+            // Lê o arquivo
+            FileReader ler = new FileReader("C:\\Users\\cesmac.ACADEMICO\\IdeaProjects\\Gustavo.aulapoo03\\cesmac-poo-2022-2\\Prova03\\proposals.txt");
+            BufferedReader reader = new BufferedReader(ler);
+            String linha;
+            while( (linha = reader.readLine()) != null ){
+                System.out.println(linha);
+            }
+
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
 }
