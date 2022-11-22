604bb3b214c7d1bd66dcddf4e743585dec330243
lucasfidelis07 <lucasfidelismcz@gmail.com>
2022-11-22 14:56:27 -0300

    Alterado o FileReader onde, antes estava para ler em uma máquina específica, agora está para ler em todas

--- Prova03/src/Evento.java
+++ Prova03/src/Evento.java
@@ -5,15 +5,13 @@ import java.io.IOException;
 public class Evento {
     public static void main(String[] args) {
         try {
-
-            // Lê o arquivo
-            FileReader ler = new FileReader("C:\\Users\\cesmac.ACADEMICO\\IdeaProjects\\Gustavo.aulapoo03\\cesmac-poo-2022-2\\Prova03\\proposals.txt");
+            // Ler o arquivo
+            FileReader ler = new FileReader("cesmac-poo-2022-2/Prova03/proposals.txt");
             BufferedReader reader = new BufferedReader(ler);
             String linha;
             while( (linha = reader.readLine()) != null ){
                 System.out.println(linha);
             }
-
 
         } catch (IOException e) {
             e.printStackTrace();
