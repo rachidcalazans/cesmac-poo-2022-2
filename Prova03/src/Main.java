010f50648f7bfc520099e37c21503315d344d92d
matheuscmc <matheuscmcastro@gmail.com>
2022-11-22 20:13:40 -0300

    Precisei refazer a branch, junto com o código, para resolver um bug do mesmo. Já tinha feito o PR na aula passada

---
+++ Prova03/src/Main.java
@@ -0,0 +1,10 @@
+
+public class Main {
+
+    public static void main(String[] args) {
+        final TrackGenerator generator = new TrackGenerator();
+
+        generator.printTracks();
+    }
+
+}
